fdcada20ece567ff2df35b6b5fe08ef130abdd33
Sonal Singh <singhsonals999@gmail.com>
2025-07-22 12:43:04 +0530

    Version 0.0.1

--- K_Prediction/k_prediction.py
+++ K_Prediction/k_prediction.py
@@ -2,9 +2,9 @@ import pickle
 
 def K_prediction(N,Temp,RH,Ph,Rainfall,P):
 
-    model = pickle.load(open("K_pred_model.pkl", "rb"))
+    model = pickle.load(open("K_Prediction/K_pred_model.pkl", "rb"))
 
-    scaler = pickle.load(open("K_pred_scale.pkl", "rb"))
+    scaler = pickle.load(open("K_Prediction/K_pred_scale.pkl", "rb"))
 
 
     Y_pred = model.predict(scaler.transform([[N,Temp,RH,Ph,Rainfall,P]]))
